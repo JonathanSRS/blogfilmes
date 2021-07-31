4dfef3b257929ab646450bb52f2916167896adae
Jonathan da Silva <jonathansilva15sp@gmail.com>
2021-07-30 22:07:37 -0300

    exercício 30/07/2021

--- src/app/service/auth.service.ts
+++ src/app/service/auth.service.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment.prod';
 import { Login } from '../model/Login';
 import { Usuario } from '../model/Usuario';
 
@@ -18,5 +19,13 @@ export class AuthService {
   }
   cadastrar(usuario: Usuario): Observable<Usuario>{
     return this.http.post<Usuario>('https://blogfilmes.herokuapp.com/usuarios/cadastrar', usuario)
+  }
+
+  logado(){
+    let ok: boolean = false
+    if(environment.token != ''){
+      ok = true
+    }
+    return ok
   }
 }
