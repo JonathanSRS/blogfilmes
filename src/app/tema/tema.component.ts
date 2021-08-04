4765d81631a2b32e5ffb792c69b81422743c8e1c
Jonathan da Silva <jonathansilva15sp@gmail.com>
2021-08-04 18:43:56 -0300

    adicionando put delete

---
+++ src/app/tema/tema.component.ts
@@ -0,0 +1,45 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { environment } from 'src/environments/environment.prod';
+import { Tema } from '../model/Tema';
+import { TemaService } from '../service/tema.service';
+
+@Component({
+  selector: 'app-tema',
+  templateUrl: './tema.component.html',
+  styleUrls: ['./tema.component.css']
+})
+export class TemaComponent implements OnInit {
+
+  tema: Tema = new Tema()
+  listaTema: Tema[]
+
+  constructor(
+    private router: Router, private temaService: TemaService
+  ) { }
+
+  ngOnInit() {
+    if(environment.token == ''){
+      alert('Sua seção expirou')
+      this.router.navigate(['/inicio'])
+
+      this.findAllTemas()
+    }
+  }
+
+  findAllTemas(){
+    this.temaService.getTema(this.tema).subscribe((resp: Tema[]) => {
+        this.listaTema = resp
+     })
+   }
+
+cadastrar(){
+  this.temaService.postTema(this.tema).subscribe((resp: Tema) => {
+    this.tema = resp
+    console.log(this.tema) 
+    alert('Tema cadastrado com sucesso!')
+    this.findAllTemas()
+    this.tema = new Tema()
+  })
+}
+}
